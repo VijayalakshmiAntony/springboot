c3b956de2e595f16a6516abdb1e3d5b6dc1fec40
VijayalakshmiAntony <43640034+VijayalakshmiAntony@users.noreply.github.com>
2025-08-31 17:26:17 +0530

    studentms project
    This project has one controller for get,put,post,delete (CURD) mapping which connects StudentDetails Entity class through JPA repo.

---
+++ src/test/java/com/schoolmanagement/software/SoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolmanagement.software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
